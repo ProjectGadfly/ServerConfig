10c99c50186f1731ecdaf89e326d83e2d8bc046a
Ziwen Chen <arthurhero@Ziwens-MacBook.local>
2017-03-26 00:13:44 -0500

    modified shell script

--- serverconfig.sh
+++ serverconfig.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt-get install apache2 libapache2-mod-wsgi-py3 python3-flask python3.5 python3.5-venv python3-requests python3-pip python3-bs4 git
+sudo apt-get install apache2 libapache2-mod-wsgi-py3 python3-flask python3.5 python3.5-venv python3-requests python3-pip python3-bs4
 cd /var/www/
-sudo git clone https://github.com/ProjectGadfly/ServerConfig.git
 sudo mkdir GFServer
 sudo mv ServerConfig/* GFServer/ 
 sudo rm -R ServerConfig/
